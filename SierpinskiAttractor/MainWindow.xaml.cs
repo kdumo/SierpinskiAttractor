70883e27663c544c9301a947c4a9835853e96b54
Cheweh <joseph.pena.943@my.csun.edu>
2014-11-22 22:14:46 -0800

    Draggin and max 6 points, AND MORE

--- SierpinskiAttractor/MainWindow.xaml.cs
+++ SierpinskiAttractor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace SierpinskiAttractor
 {
@@ -21,15 +22,17 @@ namespace SierpinskiAttractor
     public partial class MainWindow : Window
     {
         //private SOMETHING currColor;
-        //private String currShape = "Circle"; 
+        //private String shapeType = "Circle"; 
         //default to circle
         private enum selectableShapes { Circle, Rectangle }
-        private selectableShapes currShape = selectableShapes.Circle;//add more later maybe...or not
+        private selectableShapes shapeType = selectableShapes.Circle;//add more later maybe...or not
         private double mx, my;   //mouse x and y in canvas
-        private double[] sx, sy;   //captured shape x and y
-        private Shape[6] controlPoints;
-        private int shapeIndex=0;
-        private Shape selectedPoint;
+        private double[] sx = new double[6]; 
+        private double[] sy = new double[6];   //shape x and y
+        private Shape[] controlPoints = new Shape[6];
+        private int shapeIndex = 0;
+        private int cpCounter = 0;
+        private Shape selectedPoint = null;
         private bool captured = false;
         public MainWindow()
         {
@@ -53,59 +56,90 @@ namespace SierpinskiAttractor
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
+            cpCounter = 0;
+            sx = new double[6];
+            sy = new double[6];
+            controlPoints = new Shape[6];
 
         }
 
         private void DaCanvas_MouseMove_1(object sender, MouseEventArgs e)
         {
-            mx = e.GetPosition(DaCanvas).X;
-            my = e.GetPosition(DaCanvas).Y;
+            double x = e.GetPosition(DaCanvas).X;
+            double y = e.GetPosition(DaCanvas).Y;
+            if (captured)
+            {
+                sx[shapeIndex] += x - mx;
+                Canvas.SetLeft(selectedPoint, sx[shapeIndex]);
+                mx = x;
+                sy[shapeIndex] += y - my;
+                Canvas.SetTop(selectedPoint,sy[shapeIndex]);
+                my = y;
+            }
+            else
+                mousePos.Content = String.Format("Mouse at ({0},{1})", x, y);
             
         }
 
         private void setControlPoint(object sender, MouseButtonEventArgs e)
         {
-            Shape shape = null;
-            //forr debug
+            Shape newShape;
+            //forr size debug
             int size = 10;
-            switch (currShape)
-            {
-                case selectableShapes.Circle:
-                    shape = new Ellipse()
-                    {
-                        Height = size,
-                        Width = size,
-                        Fill = Brushes.Blue
-                        //we will get values from buttons/boxes
-                        //I fixed values for now
-                    };break;
-                case selectableShapes.Rectangle:
-                    shape = new Rectangle(){
-                        Height = size,
-                        Width = size,
-                        Fill = Brushes.Pink
-                    };
-                    break;
-                default:
-                    return;
+            if(cpCounter < 6){
+                String index = "Shape"+cpCounter;
+                switch (shapeType)
+                {
+                    case selectableShapes.Circle:
+                        newShape = new Ellipse()
+                        {
+                            Name = index,
+                            Height = size,
+                            Width = size,
+                            Fill = Brushes.Blue
+                            //we will get values from buttons/boxes
+                            //I fixed values for now
+                        };break;
+                    case selectableShapes.Rectangle:
+                        newShape = new Rectangle(){
+                            Name = index,
+                            Height = size,
+                            Width = size,
+                            Fill = Brushes.Pink
+                        };
+                        break;
+                    default:
+                        return;
+                }
+
+                Canvas.SetLeft(newShape, e.GetPosition(DaCanvas).X);
+                Canvas.SetTop(newShape, e.GetPosition(DaCanvas).Y);
+                DaCanvas.Children.Add(newShape);
+
+                //save for reference
+                controlPoints[cpCounter] = newShape;
+                sx[cpCounter] = Canvas.GetLeft(newShape);
+                sy[cpCounter] = Canvas.GetTop(newShape);
+
+                cpCounter++;
+                Indexshape.Content = "Control Points: " + cpCounter;
             }
-            Canvas.SetLeft(shape, e.GetPosition(DaCanvas).X);
-            Canvas.SetTop(shape, e.GetPosition(DaCanvas).Y);
-            DaCanvas.Children.Add(shape);
+            else
+                StatusLabel.Content = "Only 6 points Allowed!";
         }
 
         private void circle_Checked(object sender, RoutedEventArgs e)
         {   //how to keep people from choosing both more than one radio button at a time..??
             /*var button = sender as RadioButton;
-            currShape = button.Content.ToString();*/
-            currShape = selectableShapes.Circle;
+            shapeType = button.Content.ToString();*/
+            shapeType = selectableShapes.Circle;
         }
 
         private void square_Checked(object sender, RoutedEventArgs e)
         {
             /*var button = sender as RadioButton;
-            currShape = button.Content.ToString();*/
-            currShape = selectableShapes.Rectangle;
+            shapeType = button.Content.ToString();*/
+            shapeType = selectableShapes.Rectangle;
         }
 
         private void dragPoint(object sender, MouseButtonEventArgs e)
@@ -118,8 +152,11 @@ namespace SierpinskiAttractor
             }
             if (selectedPoint != null)
             {
-                Mouse.Capture(selectedPoint);
+                Mouse.Capture(selectedPoint);  //take it
                 captured = true;
+                mx = e.GetPosition(DaCanvas).X;
+                my = e.GetPosition(DaCanvas).Y;
+                for (shapeIndex = 0; shapeIndex < 6 && selectedPoint.Name != controlPoints[shapeIndex].Name; shapeIndex++) ;
 
             }
         }
@@ -128,6 +165,11 @@ namespace SierpinskiAttractor
         {
             //place point on click release
             //get mouse pos, place point on mouse pos (in canvas)
+            if (captured)
+            {
+                Mouse.Capture(null); //take nothing (release)
+                captured = false;
+            }
 
         }
     }
